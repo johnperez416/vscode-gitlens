305472e2de876a5184fb8220d496f6ac969a637c
Keith Daulton <keith.daulton@gitkraken.com>
2026-05-08 19:28:25 -0400

    Fixes unused imports

--- src/plus/chat/utils/-webview/chat.utils.ts
+++ src/plus/chat/utils/-webview/chat.utils.ts
@@ -1,9 +1,8 @@
-import { extensions } from 'vscode';
 import { wait } from '@gitlens/utils/promise.js';
 import type { ChatViewOpenOptions } from '../../../../@types/vscode.chat.js';
 import { callUsingClipboard } from '../../../../system/-webview/clipboard.js';
 import { executeCoreCommand } from '../../../../system/-webview/command.js';
-import { getHostAppName, isHostVSCode } from '../../../../system/-webview/vscode.js';
+import { getHostAppName } from '../../../../system/-webview/vscode.js';
 
 export async function openChat(
 	args: string,
